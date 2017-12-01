c070e4f11ab6d349c73afdd93776e2a123b27f77
Jakub Gejdos <gejdosjakub@gmail.com>
2017-12-01 18:11:35 +0100

    interim

--- DniTyzdna/DniTyzdna.cs
+++ DniTyzdna/DniTyzdna.cs
@@ -73,7 +73,9 @@ namespace DniTyzdna
             
             if (int.TryParse(den, out cisloDna))
             {
-                if (int.Parse(den) > 0 && int.Parse(den) < 8)
+                
+
+                if (int.Parse(den) >= 1 && int.Parse(den) <= 7)
                 {
                     return true;
                 }
